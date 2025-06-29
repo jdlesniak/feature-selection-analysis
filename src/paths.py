d90959fe808378f4a4e84867263362096688a67f
jdlesniak <johndlesniak@gmail.com>
2025-06-29 11:09:47 -0400

    ADDED eda notbook to guide the cleaning. UPDATED: paths.py

--- src/paths.py
+++ src/paths.py
@@ -1,20 +1,16 @@
 import os
 
-# Root directory of the project
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+from pathlib import Path
 
-# Data directories
-DATA_DIR = os.path.join(PROJECT_ROOT, "data")
-RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
-PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed")
+# Automatically find the root by going up from this file's location
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
-# Arcene files
-ARCENE_TRAIN_DATA = os.path.join(RAW_DATA_DIR, "arcene_train.data")
-ARCENE_TRAIN_LABELS = os.path.join(RAW_DATA_DIR, "arcene_train.labels")
-ARCENE_TEST_DATA = os.path.join(RAW_DATA_DIR, "arcene_valid.data")
-ARCENE_TEST_LABELS = os.path.join(RAW_DATA_DIR, "arcene_valid.labels")
+# Define standard subfolders
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+CLEAN_DATA_DIR = DATA_DIR / "clean"
 
-# Outputs (optional)
-OUTPUT_DIR = os.path.join(PROJECT_ROOT, "outputs")
-MODELS_DIR = os.path.join(OUTPUT_DIR, "models")
-PLOTS_DIR = os.path.join(OUTPUT_DIR, "plots")
+## Define data
+SEISMIC_DATA = RAW_DATA_DIR / "seismic-bumps.arff"
+
+SEED = 202506
